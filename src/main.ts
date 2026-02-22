a6bddd7593fad76cc7673ebfd731c40e8e4f67cc
Henrichy <henrynzekwe25@gmail.com>
2026-02-23 00:10:42 +0100

    CORS fixed

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,12 @@ async function bootstrap() {
   
   // Enable CORS
   app.enableCors({
-    origin: ['http://localhost:3001', 'http://localhost:3000'],
+    origin: [
+      'http://localhost:3001', 
+      'http://localhost:3000',
+      'https://tarpai-front.onrender.com',
+      process.env.FRONTEND_URL
+    ].filter(Boolean),
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
