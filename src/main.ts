67d468c77cb8b0ca5311e70a1c3782a98e6b4558
Henrichy <henrynzekwe25@gmail.com>
2026-03-15 14:09:05 +0100

    De

--- src/main.ts
+++ src/main.ts
@@ -18,7 +18,7 @@ async function bootstrap() {
     allowedHeaders: ['Content-Type', 'Authorization'],
   });
   
-  const port = process.env.PORT || 3000;
+  const port = process.env.PORT || 5000;
   await app.listen(port);
   console.log(`Application is running on: http://localhost:${port}`);
 }
